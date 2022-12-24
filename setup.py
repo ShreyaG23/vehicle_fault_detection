7d958ab65e4daf01b1132db5df69739a36659ecf
ShreyaG23 <shreygold27@gmail.com>
2022-12-24 14:08:18 +0530

    updating setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+def get_requirements():
+    pass
+
+setup(
+    name = "sensor",
+    version = "0.0.1",
+    author = "Shreya",
+    author_email= "shreygold27@.gmail",
+    packages=find_packages(),
+    install_requires = get_requirements(),
+)
